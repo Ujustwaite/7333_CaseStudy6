681620a241e2599c77eabcfceb738fdad5a5253e
Ujustwaite <Ujustwaite@gmail.com>
2020-02-17 19:56:03 -0500

    Final code used in paper

--- CaseStudy6.r
+++ CaseStudy6.r
@@ -38,13 +38,22 @@ trainDF =
   rbind( emailDFrp[emailDFrp$isSpam == "T", ][-testSpamIdx, ], 
          emailDFrp[emailDFrp$isSpam == "F", ][-testHamIdx, ])
 
+#Check the numbers in each
+sum(trainDF$isSpam == "T") 
+sum(trainDF$isSpam == "F")
+length(trainDF$isSpam)
+
+sum(testDF$isSpam == "T") 
+sum(testDF$isSpam == "F")
+length(testDF$isSpam)
+
 #####Baseline (Provided) Model and Parameters######
 
 #Fit the default / baseline model with all features
 rpartFit = rpart(isSpam ~ ., data = trainDF, method = "class")
 
 #plot the tree created
-rpart.plot(rpartFit, extra = 1)
+rpart.plot(rpartFit, extra = 1, cex = .4)
 
 #make predictions on the test set
 predictions = predict(rpartFit, 
@@ -91,7 +100,7 @@ errs = sapply(fits, function(preds) {
 cols = brewer.pal(9, "Set1")[c(3, 4, 5)]
 plot(errs[1,] ~ complexityVals, type="l", col=cols[2], 
      lwd = 2,   ylim = c(0,0.2), xlim = c(0,0.01), 
-     ylab="Error", xlab="complexity parameter values")
+     ylab="Error", xlab="complexity parameter (cp) values", main = "Error Plot over multiple CP Values")
 points(errs[2,] ~ complexityVals, type="l", col=cols[1], lwd = 2)
 
 text(x =c(0.003, 0.0035), y = c(0.12, 0.05), 
@@ -158,7 +167,7 @@ errs = sapply(fits, function(preds) {
 cols = brewer.pal(9, "Set1")[c(3, 4, 5)]
 plot(errs[1,] ~ minSplitVals, type="l", col=cols[2], 
      lwd = 2, ylim = c(0,.21), xlim = c(0,30), 
-     ylab="Error", xlab="min split values")
+     ylab="Error", xlab="Minimum split (minsplit) values", main = "Error Plot over Multiple Values of minsplit (1:30)")
 points(errs[2,] ~ minSplitVals, type="l", col=cols[1], lwd = 2)
 
 text(x =c(1, 1), y = c(0.12, 0.05), 
@@ -178,7 +187,7 @@ text(minSplitVals[minI]+1, errs[2, minI]+0.01,
 #Find the value of the best cp from these errors
 minSplitVals[minI]
 
-#Looks like it is 0.0015 (which we could estimate off the plot)
+#Looks like it is 23 
 
 #Fit the model with this updated parameter
 rpartFit = rpart(isSpam ~ ., data = trainDF, method = "class", control = rpart.control(cp = 0.0015, minsplit = 23))
@@ -204,8 +213,8 @@ matrix
 
 ##########In theory the more folds the better, but performance may be an issue##################
 
-#Create a search range of 1 to 30. 
-numFolds = c(seq(1,30))
+#Create a search range of 1 to 50. 
+numFolds = c(seq(1,50))
 
 
 
@@ -228,8 +237,8 @@ errs = sapply(fits, function(preds) {
 #Plot the Type 1 and Type 2 Errors
 cols = brewer.pal(9, "Set1")[c(3, 4, 5)]
 plot(errs[1,] ~ numFolds, type="l", col=cols[2], 
-     lwd = 2, ylim = c(0,.21), xlim = c(0,30), 
-     ylab="Error", xlab="min split values")
+     lwd = 2, ylim = c(0,.21), xlim = c(0,50), 
+     ylab="Error", xlab="Number of Cross Validation Folds", main = "Error Plot using multiple values for xval folding")
 points(errs[2,] ~ numFolds, type="l", col=cols[1], lwd = 2)
 
 text(x =c(1, 1), y = c(0.12, 0.05), 
@@ -249,10 +258,11 @@ text(numFolds[minI]+1, errs[2, minI]+0.01,
 #Find the value of the best cp from these errors
 numFolds[minI]
 
-#Looks like it is 0.0015 (which we could estimate off the plot)
+#Huh....no difference. After checking the code above, it appears that the model simply performs the same across all folds. We'll just leave it at the 
+#default value of 10. 
 
 #Fit the model with this updated parameter
-rpartFit = rpart(isSpam ~ ., data = trainDF, method = "class", control = rpart.control(cp = 0.0015, minsplit = 23, xval = 20))
+rpartFit = rpart(isSpam ~ ., data = trainDF, method = "class", control = rpart.control(cp = 0.0015, minsplit = 23, xval = 10))
 
 #plot the tree created
 rpart.plot(rpartFit, extra = 1)
@@ -320,7 +330,7 @@ errs = sapply(fits, function(preds) {
 cols = brewer.pal(9, "Set1")[c(3, 4, 5)]
 plot(errs[1,] ~ depths, type="l", col=cols[2], 
      lwd = 2, ylim = c(0,.6), xlim = c(0,30), 
-     ylab="Error", xlab="min split values")
+     ylab="Error", xlab="maxdepth values", main = "Errors Plotted Across Multiple Values for maxdepth")
 points(errs[2,] ~ depths, type="l", col=cols[1], lwd = 2)
 
 text(x =c(1, 1), y = c(0.4, 0.12), 
@@ -337,16 +347,18 @@ text(depths[minI]+1, errs[2, minI]+0.01,
 
 #And get the best value for reducing the type 1 error
 
-#Find the value of the best cp from these errors
+#Find the value of the best maxdepth from these errors
 depths[minI]
 
-#Looks like it is 0.0015 (which we could estimate off the plot)
+#Wow....it looks like 3 is the right value, but that leaves us with high Type 2 error. Maybe that isn't desireable if we can get similar 
+#results by using a maxdepth of 10. 
 
 #Fit the model with this updated parameter
 rpartFit = rpart(isSpam ~ ., data = trainDF, method = "class", control = rpart.control(cp = 0.0015, minsplit = 23, xval = 10, maxdepth = 3))
 
 #plot the tree created
-rpart.plot(rpartFit, extra = 1)
+rpart.plot(rpartFit, extra = 1, main = "Spam Decision Tree with Max Depth of 3")
+
 
 #make predictions on the test set
 predictions = predict(rpartFit, 
@@ -365,6 +377,73 @@ rpartFit = rpart(isSpam ~ ., data = trainDF, method = "class", control = rpart.c
 
 #plot the tree created
 rpart.plot(rpartFit, extra = 1)
+
+#make predictions on the test set
+predictions = predict(rpartFit, 
+                      newdata = testDF[, names(testDF) != "isSpam"],
+                      type = "class")
+
+#Display the confusion matrix for these values
+matrix = confusionMatrix(predictions, testDF$isSpam, positive = "T")
+matrix
+
+
+############Let's try messing with minbucket###################
+
+
+#Default is minsplits / 3, so currently 7.6666
+
+#Create a search range of 1 to 30. 
+buckets = c(seq(1,30))
+
+
+
+fits = lapply(buckets, function(x) {
+  rpartObj = rpart(isSpam ~ ., data = trainDF,
+                   method="class", 
+                   control = rpart.control(cp=0.0015, minsplit = 23, xval = 10, maxdepth = 10, minbucket = x) )
+  
+  predict(rpartObj, 
+          newdata = testDF[ , names(testDF) != "isSpam"],
+          type = "class")
+})
+
+errs = sapply(fits, function(preds) {
+  typeI = sum(preds[ !spam ] == "T") / numHam
+  typeII = sum(preds[ spam ] == "F") / numSpam
+  c(typeI = typeI, typeII = typeII)
+})
+
+#Plot the Type 1 and Type 2 Errors
+cols = brewer.pal(9, "Set1")[c(3, 4, 5)]
+plot(errs[1,] ~ depths, type="l", col=cols[2], 
+     lwd = 2, ylim = c(0,.6), xlim = c(0,30), 
+     ylab="Error", xlab="maxdepth values", main = "Errors Plotted Across Multiple Values for minbucket")
+points(errs[2,] ~ depths, type="l", col=cols[1], lwd = 2)
+
+text(x =c(1, 1), y = c(0.4, 0.12), 
+     labels=c("Type II Error", "Type I Error"))
+
+minI = which(errs[1,] == min(errs[1,]))[1]
+abline(v = depths[minI], col ="grey", lty =3, lwd=2)
+
+text(depths[minI]+1, errs[1, minI]+0.01, 
+     formatC(errs[1, minI], digits = 2))
+text(depths[minI]+1, errs[2, minI]+0.01, 
+     formatC(errs[2, minI], digits = 3))
+
+
+#And get the best value for reducing the type 1 error
+
+#Find the value of the best minbucket from these errors
+depths[minI]
+
+#Fit the model with this updated parameter
+rpartFit = rpart(isSpam ~ ., data = trainDF, method = "class", control = rpart.control(cp = 0.0015, minsplit = 23, xval = 10, maxdepth = 10, minbucket = 9))
+
+#plot the tree created
+rpart.plot(rpartFit, extra = 1, main = "Spam Decision Tree with minbucket of 9")
+
 
 #make predictions on the test set
 predictions = predict(rpartFit, 
